0adeeb10a250c559cc475aa00971e2852e6e3dfe
lunc <cwljay1991@126.com>
2018-01-08 11:25:17 +0800

    react-native-camera配置

--- sewerage/app/project/modules/main/message.js
+++ sewerage/app/project/modules/main/message.js
@@ -2,26 +2,44 @@ import React from 'react';
 import {
     View,
     Text,
+    StyleSheet
+    // Button,
 } from 'react-native';
-import {WrapScreen} from "../wrap";
-import * as Assert from '../../assets'
+import { WrapScreen } from "../wrap";
+import * as Assert from '../../assets';
+import Camera from 'react-native-camera';
 
 export class MessageScreen extends WrapScreen {
     constructor(props) {
         super(props);
     }
 
+    _onBarCodeRead = (e) => {
+        console.log("Barcode Found!",
+            "Type: " + e.type + "\nData: " + e.data)
+    }
+
     _render() {
         return (
-            <View>
-                <Text>sssssss</Text>
-                <Text>sssssss</Text>
-                <Text>sssssss</Text>
-                <Text>sssssss</Text>
-                <Text>sssssss</Text>
-                <Text>sssssss</Text>
-                <Text>sssssss</Text>
+            <View style={styles.container}>
+                <Camera
+                    style={styles.camera}
+                    ref={(cam) => {
+                        this.camera = cam;
+                    }}
+                    onBarCodeRead={this._onBarCodeRead.bind(this)}></Camera>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+      },
+    camera: {
+        flex:1,
+        backgroundColor:'red',
+    }
+})
