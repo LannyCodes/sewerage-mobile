8488bba1cdb892bdbb5f9c3b4f207d05f8dcadce
铉嘉伟 <mifind@sina.com>
2018-01-11 15:03:53 +0800

    修改一个文件名而已

--- sewerage/app/project/modules/task/index.js
+++ sewerage/app/project/modules/task/index.js
@@ -1,5 +1,5 @@
 import tasks from './tasks';
-import maintenanceDetail from './detail';
+import maintenanceDetail from './maintenanceDetail';
 
 export const TaskListScreen = tasks;
 export const TaskMaintenanceDetailScreen = maintenanceDetail;
